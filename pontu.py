24cb3b71eed2992fd07d12097b202067da43ba9b
JPLdeCamargo <jplopescamargo@gmail.com>
2022-09-16 16:28:19 -0300

    images can be resized

--- pontu.py
+++ pontu.py
@@ -1,51 +1,46 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 class PlayerActor:
     def __init__(self):
         self.__main_window = tk.Tk()
         self.__main_window.title("Pontu!!")
-        self.__main_window.geometry("450x450")
+        self.__size = 2
+        self.__main_window.geometry(f"{450*self.__size}x{450*self.__size}")
         self.__main_window["bg"] = "red"
         self.__is_crown_turn = True
         self.__main_frame = tk.Frame(self.__main_window,
                                      bg="red")
 
         # pyimage1
-        self.__empty_rock = tk.PhotoImage(file="images/rock.png")
+        self.__empty_rock = self.resize("images/rock.png")
         # pyimage2
-        self.__v_bridge = tk.PhotoImage(file="images/bridge_v.png")
+        self.__v_bridge = self.resize("images/bridge_v.png")
         # pyimage3
-        self.__h_bridge = tk.PhotoImage(file="images/bridge_h.png")
+        self.__h_bridge = self.resize("images/bridge_h.png")
         # pyimage4
-        self.__water = tk.PhotoImage(file="images/water.png")
+        self.__water = self.resize("images/water.png")
         # pyimage5
-        self.__skull_rock = tk.PhotoImage(file="images/skull_rock.png")
+        self.__skull_rock = self.resize("images/skull_rock.png")
         # pyimage6
-        self.__crown_rock = tk.PhotoImage(file="images/crown_rock.png")
+        self.__crown_rock = self.resize("images/crown_rock.png")
 
         self.__board_view = []
         for y in range(9):
             line = []
             for x in range(9):
+                img = None
                 if x % 2 == 0 and y % 2 == 0:
-                    label = tk.Label(self.__main_frame,
-                                     bd=0,
-                                     relief="solid",
-                                     image=self.__empty_rock)
+                    img = self.__empty_rock 
                 elif y % 2 == 0:
-                    label = tk.Label(self.__main_frame,
-                                     bd=0,
-                                     relief="solid",
-                                     image=self.__v_bridge)
+                    img = self.__v_bridge
                 elif x % 2 == 0:
-                    label = tk.Label(self.__main_frame,
-                                     bd=0,
-                                     relief="solid",
-                                     image=self.__h_bridge)
+                    img = self.__h_bridge
                 else:
-                    label = tk.Label(self.__main_frame,
-                                     bd=0,
-                                     relief="solid",
-                                     image=self.__water)
+                    img = self.__water 
+                label = tk.Label(self.__main_frame,
+                                 bd=0,
+                                 relief="solid",
+                                 image=img)
                 label.grid(row = x, column = y)
                 label.bind("<Button-1>", lambda event, line = y+1,
                         column=x+1: self.click(event, line, column))
@@ -63,5 +58,11 @@ class PlayerActor:
             replace = self.__crown_rock if self.__is_crown_turn else self.__skull_rock
             label['imag'] = replace
             self.__is_crown_turn = not self.__is_crown_turn
+
+    def resize(self, file_path:str):
+        img = Image.open(file_path)
+        img = img.resize((50*self.__size, 50*self.__size))
+        img = ImageTk.PhotoImage(img)
+        return img
 
 PlayerActor()
